92b85b228ec2208cde85b3194603117ab37afbc2
Kim <kimviggedal@gmail.com>
2019-10-11 23:28:58 +0200

    Add comprehensive alcohol price info

--- apk.py
+++ apk.py
@@ -1,10 +1,10 @@
 import requests
 import xml.etree.ElementTree as ET
-import xml.dom.minidom
+import pprint
 
-URL = "https://www.systembolaget.se/api/assortment/products/xml"
+pp = pprint.PrettyPrinter(indent=4)
 
-response = requests.get(URL)
+response = requests.get("https://www.systembolaget.se/api/assortment/products/xml")
 with open('feed.xml', 'wb') as file:
     file.write(response.content)
 
@@ -14,5 +14,13 @@ root = tree.getroot()
 articles = root.findall('artikel')
 
 for article in articles:
-    price = article.find('PrisPerLiter')
-    print(price.text)
+    product_name = article.find('Namn').text
+    price = article.find('PrisPerLiter').text
+    strength = article.find('Alkoholhalt').text
+    price_numeric = float(price)
+    strength_numeric = float(strength[0:4]) / float(100)
+    if(strength_numeric != 0):
+        alcohol_price = price_numeric / strength_numeric
+    else:
+        print('No alcohol?! WTF?!')
+    print('Product name: ', product_name, ', Price: ', price, 'Alcohol price: ', alcohol_price)
