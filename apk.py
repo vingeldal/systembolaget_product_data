e37b1d82de3021563332e50135bda77f34278725
Kim <kimviggedal@gmail.com>
2019-10-11 21:23:20 +0200

    print out articles from xml file

--- apk.py
+++ apk.py
@@ -1,7 +1,14 @@
 import requests
+import xml.etree.ElementTree as ET
 
 URL = "https://www.systembolaget.se/api/assortment/products/xml"
 
 response = requests.get(URL)
 with open('feed.xml', 'wb') as file:
     file.write(response.content)
+
+tree = ET.parse('feed.xml')
+root = tree.getroot()
+
+for child in root:
+    print(child.tag, child.attrib)
